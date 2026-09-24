fb972b1fcf11c75edd3fa9605ea9d8ff92c16d7b
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2026-09-24 22:37:59 +0200

    core(api url): added remote API base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
-import {environmentCommon} from './environment.common';
-import {Environment} from './environment.model';
+import { environmentCommon } from './environment.common';
+import { Environment } from './environment.model';
 
 export const environment: Environment = {
   ...environmentCommon,
+  apiUrl: 'https://us-central1-digital-book-fbaa0.cloudfunctions.net/api',
 };
